88480b98473fcadd6f793d1201cb2fb480e9f6d7
afoisl <160198328+afoisl@users.noreply.github.com>
2024-12-28 18:56:05 +0900

    [ feat ] 대기 생성 요청 DTOv1 생성 ( #1 )

---
+++ src/main/java/com/qring/queue/presentation/v1/req/PostQueueReqDTOv1.java
@@ -0,0 +1,16 @@
+package com.qring.queue.presentation.v1.req;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class PostQueueReqDTOv1 {
+
+    private Queue queue;
+
+    @Getter
+    public static class Queue {
+        private Long reservationId;
+    }
+}
