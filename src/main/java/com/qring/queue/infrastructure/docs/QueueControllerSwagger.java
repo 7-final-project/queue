464d43b762ee857c3109d1b23cfdb8bbbaec7995
afoisl <160198328+afoisl@users.noreply.github.com>
2024-12-30 23:37:02 +0900

    [ refactor ] Swagger 문서화 수정 ( #1 )

--- src/main/java/com/qring/queue/infrastructure/docs/QueueControllerSwagger.java
+++ src/main/java/com/qring/queue/infrastructure/docs/QueueControllerSwagger.java
@@ -18,16 +18,13 @@ import org.springframework.web.bind.annotation.*;
 @Tag(name = "Queues", description = "등록, 조회, 수정, 삭제 관련 대기 API")
 public interface QueueControllerSwagger {
 
-
     @Operation(summary = "대기 등록", description = "예약 ID를 기준으로 대기를 등록하는 API 입니다.")
     @ApiResponses(value = {
             @ApiResponse(responseCode = "201", description = "대기 등록 성공", content = @Content(schema = @Schema(implementation = ResDTO.class))),
             @ApiResponse(responseCode = "400", description = "대기 등록 실패", content = @Content(schema = @Schema(implementation = ResDTO.class))),
     })
     @PostMapping("/v1/queues")
-    public ResponseEntity<ResDTO<QueuePostResDTOV1>> postBy(@RequestBody PostQueueReqDTOV1 dto);
-
-
+    ResponseEntity<ResDTO<QueuePostResDTOV1>> postBy(@RequestBody PostQueueReqDTOV1 dto);
 
     @Operation(summary = "대기 가게별 조회", description = "가게별로 대기자를 조회하는 API 입니다.")
     @ApiResponses(value = {
@@ -35,9 +32,7 @@ public interface QueueControllerSwagger {
             @ApiResponse(responseCode = "400", description = "대기 조회 실패", content = @Content(schema = @Schema(implementation = ResDTO.class))),
     })
     @GetMapping("/v1/queues")
-    public ResponseEntity<ResDTO<QueueGetTableResDTOV1>> getBy();
-
-
+    ResponseEntity<ResDTO<QueueGetTableResDTOV1>> getBy();
 
     @Operation(summary = "대기 상세 조회", description = "대기 ID 기준으로 대기 상세 정보를 조회하는 API 입니다.")
     @ApiResponses(value = {
@@ -45,9 +40,7 @@ public interface QueueControllerSwagger {
             @ApiResponse(responseCode = "400", description = "대기 조회 실패", content = @Content(schema = @Schema(implementation = ResDTO.class))),
     })
     @GetMapping("/v1/queues/{id}")
-    public ResponseEntity<ResDTO<QueueGetByIdResDTOV1>> getBy(@PathVariable Long id);
-
-
+    ResponseEntity<ResDTO<QueueGetByIdResDTOV1>> getBy(@PathVariable Long id);
 
     @Operation(summary = "대기 상태 수정", description = "대기 ID 기준으로 대기 상태를 수정하는 API 입니다.")
     @ApiResponses(value = {
@@ -55,10 +48,8 @@ public interface QueueControllerSwagger {
             @ApiResponse(responseCode = "400", description = "대기 수정 실패", content = @Content(schema = @Schema(implementation = ResDTO.class))),
     })
     @PutMapping("/v1/queues/{id}")
-    public ResponseEntity<ResDTO<Object>> putBy(@PathVariable Long id,
+    ResponseEntity<ResDTO<Object>> putBy(@PathVariable Long id,
                                            @RequestBody PutQueueReqDTOV1 dto);
-
-
 
     @Operation(summary = "대기 취소", description = "대기 ID 기준으로 대기를 취소하는 API 입니다.")
     @ApiResponses(value = {
@@ -66,5 +57,5 @@ public interface QueueControllerSwagger {
             @ApiResponse(responseCode = "400", description = "대기 취소 실패", content = @Content(schema = @Schema(implementation = ResDTO.class))),
     })
     @DeleteMapping("/v1/queues/{id}")
-    public ResponseEntity<ResDTO<Object>> cancelBy(@PathVariable Long id);
+    ResponseEntity<ResDTO<Object>> cancelBy(@PathVariable Long id);
 }
