d8e6ccb7667198c1deef64360b79587edfbe7c99
afoisl <160198328+afoisl@users.noreply.github.com>
2025-01-06 13:01:57 +0900

    [ refactor ] DTO 명 변경 ( #7 )

--- src/main/java/com/qring/queue/infrastructure/messaging/dto/ReservationCreateEventDTO.java
+++ src/main/java/com/qring/queue/infrastructure/messaging/dto/ReservationCreateEventDTO.java
@@ -1,11 +1,11 @@
-package com.qring.queue.infrastructure.messaging;
+package com.qring.queue.infrastructure.messaging.dto;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
-public class ReservationCreateEvent {
+public class ReservationCreateEventDTO {
     private Long reservationId;
     private Long restaurantId;
 }
