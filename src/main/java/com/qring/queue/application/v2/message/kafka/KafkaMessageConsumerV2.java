89e8730760ef9abab8a05ad3c77dbc214242d72f
afoisl <160198328+afoisl@users.noreply.github.com>
2025-01-06 14:04:31 +0900

    [ refactor ] DTO 명 변경 ( #7 )

--- src/main/java/com/qring/queue/application/v2/message/kafka/KafkaMessageConsumerV2.java
+++ src/main/java/com/qring/queue/application/v2/message/kafka/KafkaMessageConsumerV2.java
@@ -1,7 +1,5 @@
 package com.qring.queue.application.v2.message.kafka;
 
-import com.qring.queue.infrastructure.messaging.dto.ReservationCreateEventDTO;
-
 public interface KafkaMessageConsumerV2 {
-    ReservationCreateEventDTO handleMessage(String message);
+    void extractIdListBy(String message);
 }
--- src/main/java/com/qring/queue/infrastructure/messaging/kafka/KafkaMessageConsumerImplV2.java
+++ src/main/java/com/qring/queue/infrastructure/messaging/kafka/KafkaMessageConsumerImplV2.java
@@ -2,6 +2,7 @@ package com.qring.queue.infrastructure.messaging.kafka;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.qring.queue.application.v2.message.kafka.KafkaMessageConsumerV2;
+import com.qring.queue.application.v2.service.QueueServiceV2;
 import com.qring.queue.infrastructure.messaging.dto.ReservationCreateEventDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,15 +14,17 @@ import org.springframework.stereotype.Component;
 @Slf4j(topic = "KafkaMessageConsumerV2 Log")
 public class KafkaMessageConsumerImplV2 implements KafkaMessageConsumerV2 {
 
+    private final QueueServiceV2 queueServiceV2;
+
     // Kafka 메시지 처리
     @KafkaListener(topics = "reservation-create-event-topic", groupId = "${spring.kafka.consumer.group-id}")
-    public ReservationCreateEventDTO handleMessage(String message) {
+    public void extractIdListBy(String message) {
         try {
             // 메시지에서 예약 ID와 식당 ID 추출
-            return parseMessage(message);
+            ReservationCreateEventDTO event = parseMessage(message);
+            queueServiceV2.enrollWaitingListBy(event);
         } catch (Exception e) {
             log.error("메시지 추출 실패 : {}", message, e);
-            throw new RuntimeException("Kafka 메시지 처리 실패: " + message, e);
         }
     }
 
