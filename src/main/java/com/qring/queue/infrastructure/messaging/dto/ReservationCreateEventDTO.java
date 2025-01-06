f1c9936e5a255a40dadeb9b158ca3e627472e54b
afoisl <160198328+afoisl@users.noreply.github.com>
2025-01-06 21:25:26 +0900

    [ refactor ] @JsonProperty 추가 ( #8 )

--- src/main/java/com/qring/queue/infrastructure/messaging/dto/ReservationCreateEventDTO.java
+++ src/main/java/com/qring/queue/infrastructure/messaging/dto/ReservationCreateEventDTO.java
@@ -1,11 +1,15 @@
 package com.qring.queue.infrastructure.messaging.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Getter
 @NoArgsConstructor
 public class ReservationCreateEventDTO {
+    @JsonProperty("id")
     private Long reservationId;
+
+    @JsonProperty("restaurantId")
     private Long restaurantId;
 }
