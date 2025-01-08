428c448b68191ed5397cf2a6875fb8a97ab2e0c4
afoisl <160198328+afoisl@users.noreply.github.com>
2025-01-08 22:53:18 +0900

    [ feat ] 예약 정보 + 대기 정보 로직 구현 ( #16 )

--- src/main/java/com/qring/queue/application/v2/service/QueueServiceV2.java
+++ src/main/java/com/qring/queue/application/v2/service/QueueServiceV2.java
@@ -2,7 +2,7 @@ package com.qring.queue.application.v2.service;
 
 import com.qring.queue.application.v2.message.redis.RedisMessagePublisherV2;
 import com.qring.queue.application.v2.res.QueueGetResDTOV2;
-import com.qring.queue.infrastructure.messaging.dto.ReservationCreateEventDTO;
+import com.qring.queue.infrastructure.messaging.dto.ReservationEventDTOV2;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -17,7 +17,7 @@ public class QueueServiceV2 {
     private final RedisMessagePublisherV2 redisMessagePublisherV2;
 
     // 대기열 등록
-    public void enrollWaitingListByEvent(ReservationCreateEventDTO event) {
+    public void enrollWaitingListByEvent(ReservationEventDTOV2 event) {
 
         // Redis에 예약 정보 저장
         redisMessagePublisherV2.addWaitingListByRestaurantIdAndReservationId(event.getRestaurantId(), event.getReservationId());
--- src/main/java/com/qring/queue/infrastructure/messaging/kafka/KafkaMessageConsumerV2.java
+++ src/main/java/com/qring/queue/infrastructure/messaging/kafka/KafkaMessageConsumerV2.java
@@ -1,8 +1,10 @@
 package com.qring.queue.infrastructure.messaging.kafka;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.qring.queue.application.v2.message.kafka.KafkaMessageProducerV2;
+import com.qring.queue.application.v2.res.QueueGetResDTOV2;
 import com.qring.queue.application.v2.service.QueueServiceV2;
-import com.qring.queue.infrastructure.messaging.dto.ReservationCreateEventDTO;
+import com.qring.queue.infrastructure.messaging.dto.ReservationAndQueueEventDTOV2;
+import com.qring.queue.infrastructure.messaging.dto.ReservationEventDTOV2;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
@@ -14,26 +16,34 @@ import org.springframework.stereotype.Service;
 public class KafkaMessageConsumerV2 {
 
     private final QueueServiceV2 queueServiceV2;
+    private final KafkaMessageProducerV2 kafkaMessageProducerV2;
 
     // Kafka 메시지 처리
     @KafkaListener(topics = "reservation-create-event-topic", groupId = "${spring.kafka.consumer.group-id}")
-    public void extractIdListBy(String message) {
+    public void extractIdListBy(ReservationEventDTOV2 message) {
         try {
-            // 메시지에서 예약 ID와 식당 ID 추출
-            ReservationCreateEventDTO event = parseMessage(message);
-            queueServiceV2.enrollWaitingListByEvent(event);
+            QueueGetResDTOV2.QueueInfo dto = queueServiceV2.getBy(
+                    message.getRestaurantId(),
+                    message.getReservationId())
+                    .getQueueInfo();
+
+            // 새로운 대기 정보를 포함한 DTO 생성
+            ReservationAndQueueEventDTOV2 event = ReservationAndQueueEventDTOV2.builder()
+                    .reservationId(message.getReservationId())
+                    .restaurantId(message.getRestaurantId())
+                    .restaurantName(message.getRestaurantName())
+                    .restaurantTel(message.getRestaurantTel())
+                    .userId(message.getUserId())
+                    .slackEmail(message.getSlackEmail())
+                    .headCount(message.getHeadCount())
+                    .waitingNumber(dto.getSequence())
+                    .teamsAhead(dto.getTotal())
+                    .build();
+
+            // 메시지 서비스로 전송
+            kafkaMessageProducerV2.publishReservationAndQueueEvent(event);
         } catch (Exception e) {
             log.error("메시지 추출 실패 : {}", message, e);
-        }
-    }
-
-    // 예약, 식당 아이디 추출
-    private ReservationCreateEventDTO parseMessage(String message) {
-        try {
-            ObjectMapper objectMapper = new ObjectMapper();
-            return objectMapper.readValue(message, ReservationCreateEventDTO.class);
-        } catch (Exception e) {
-            throw new IllegalArgumentException("Invalid message format: " + message, e);
         }
     }
 }
