3f4fce30b721932fa7acfabf46e010baced43955
afoisl <160198328+afoisl@users.noreply.github.com>
2025-01-09 12:39:53 +0900

    [ refactor ] DTO 내용 수정 ( #16 )

--- src/main/java/com/qring/queue/infrastructure/messaging/dto/ReservationAndQueueEventDTOV2.java
+++ src/main/java/com/qring/queue/infrastructure/messaging/dto/ReservationAndQueueEventDTOV2.java
@@ -18,10 +18,10 @@ public class ReservationAndQueueEventDTOV2 {
     private Long userId;            // 사용자 ID
     private String slackEmail;      // 사용자의 슬랙 이메일
     private int headCount;          // 예약 인원수
-    private int waitingNumber;      // 대기 번호
-    private int teamsAhead;         // 내 앞에 대기 중인 팀 수
+    private String username;        // 사용자 닉네임
+    private int sequence;           // 대기 번호
 
-    public static ReservationAndQueueEventDTOV2 from(ReservationEventDTOV2 event, int waitingNumber, int teamsAhead) {
+    public static ReservationAndQueueEventDTOV2 from(ReservationCreationEventDTOV2 event, int sequence) {
         return ReservationAndQueueEventDTOV2.builder()
                 .reservationId(event.getReservationInfo().getId())
                 .restaurantId(event.getReservationInfo().getRestaurantId())
@@ -30,8 +30,7 @@ public class ReservationAndQueueEventDTOV2 {
                 .userId(event.getUserInfo().getUserId())
                 .slackEmail(event.getUserInfo().getSlackEmail())
                 .headCount(event.getReservationInfo().getHeadCount())
-                .waitingNumber(waitingNumber)
-                .teamsAhead(teamsAhead)
+                .sequence(sequence)
                 .build();
     }
 }
