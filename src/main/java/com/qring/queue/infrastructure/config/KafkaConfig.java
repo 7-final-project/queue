d9674d6e455df5909a05f14fdb43a8da64def249
afoisl <160198328+afoisl@users.noreply.github.com>
2025-01-13 20:05:35 +0900

    [ feat ] KafkaConfig 간소화 ( #20 )

---
+++ src/main/java/com/qring/queue/infrastructure/config/KafkaConfig.java
@@ -0,0 +1,16 @@
+package com.qring.queue.infrastructure.config;
+
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+
+public class KafkaConfig {
+
+    @Value("${spring.kafka.topic.queue-alarm-event}")
+    private String queueAlarmEventTopic;
+
+    @Bean
+    public NewTopic queueAlarmEventTopic() {
+        return new NewTopic(queueAlarmEventTopic, 1, (short) 1);
+    }
+}
